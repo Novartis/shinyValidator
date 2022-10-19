1e28c4fcb650f719730468397d57ac2ca97635a6
DavidNojnarg <dgranjon@ymail.com>
2022-10-19 13:06:52 +0000

    add grey status if no constraints on pkg versions

--- R/check-dependencies.R
+++ R/check-dependencies.R
@@ -67,35 +67,33 @@ get_package_data <- function(package_names, r_version, locked_deps) {
       tmp_locked <- locked_deps[locked_deps$Package == tmp$Package, ]
       # Check if package is in the provided list
       if (nrow(tmp_locked) > 0) {
-        if (tmp_locked$Version == tmp$Version) " green" else " red"
+        if (tmp_locked$Version == tmp$Version) "green" else "red"
       } else {
-        " red"
+        "red"
       }
     } else {
-      NULL
+      "grey"
     }
 
     # Avoid to capture elements like base R, utils, ...
     if (!is.null(tmp)) {
       tags$div(
-        class = paste0("card", card_status),
+        class = paste("card", card_status),
         tags$div(
           class = "content",
           tags$div(class = "header", tmp$Package),
           tags$div(
             class = "meta",
             sprintf("Current: %s", tmp$Version),
-            if (!is.null(card_status)) {
-              if (card_status == " red") {
-                if (nrow(tmp_locked) == 0) {
-                  # Package may not be available in snapshot
-                  sprintf("Not available")
-                } else {
-                  sprintf("Expected: %s", tmp_locked$Version)
-                }
+            if (card_status == "red") {
+              if (nrow(tmp_locked) == 0) {
+                # Package may not be available in snapshot
+                sprintf("Not available")
               } else {
-                "All good"
+                sprintf("Expected: %s", tmp_locked$Version)
               }
+            } else if (card_status == "green") {
+              "All good"
             }
           ),
           tags$div(
